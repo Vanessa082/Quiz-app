5b42d160a5ee0b19e1085b2d662d318bbc90d281
vanessa082 <wahvanessa22@gmail.com>
2024-01-13 08:57:16 +0100

    design the last page

--- asset/script/questioins.js
+++ asset/script/questioins.js
@@ -1,3 +1,14 @@
+//DOM
+const _question = document.getElementById('question')
+const _options = document.querySelector('.quiz-options')
+const nestBtn = document.querySelector('.next')
+const _totalQuestion = document.getElementById('total-question')
+
+// event listeners
+function eventListeners(){
+  nestBtn.addEventListener('click', nextQuestion)
+}
+
 // GETTING URL
 const url = localStorage.getItem('url')
 
